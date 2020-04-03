19ddd292b9c14ef4ff26dbdf9a5e09c08c481b26
Szentgyörgyi Máté <szentgyorgyi.mate@gmail.com>
2020-04-03 22:03:38 +0200

    Updates

---
+++ precache-manifest.953c6d1bf86cc4421baa062661f6a516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961e009d7b928f60f6b07ac8aff88b50",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "ee4cce911d4818c0cded",
+    "url": "/memory-game/static/css/main.9ce830b3.chunk.css"
+  },
+  {
+    "revision": "e1228feda003f002c4da",
+    "url": "/memory-game/static/js/2.ed84df1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.ed84df1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4cce911d4818c0cded",
+    "url": "/memory-game/static/js/main.7c0e6fd9.chunk.js"
+  },
+  {
+    "revision": "b2531d9b34d617689269",
+    "url": "/memory-game/static/js/runtime-main.8753af7e.js"
+  }
+]);
